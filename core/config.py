f78184ceab18712b671ef90a65708a4ac9087717
rezatn0934 <rezatn0934@gmail.com>
2023-10-27 11:42:09 +0330

    chore: define redis database number in settings

--- core/config.py
+++ core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
+    REDIS_DATABASE_NUMBER: int
     ACCESS_TOKEN_LIFETIME: int
     REFRESH_TOKEN_LIFETIME: int
     SECRET_KEY: str
