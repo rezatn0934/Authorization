cfeb9fff1e40b0c6986b4529115be734c5f1afab
rezatn0934 <rezatn0934@gmail.com>
2023-11-10 13:27:49 +0330

    chore: define project required configs

--- config/config.py
+++ config/config.py
@@ -5,10 +5,21 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
     REDIS_DATABASE_NUMBER: int
+
     ACCESS_TOKEN_LIFETIME: int
     REFRESH_TOKEN_LIFETIME: int
+
     SECRET_KEY: str
     ALGORITHM: str
+
+    ACCOUNT_REGISTER_URL: str
+    ACCOUNT_LOGIN_URL: str
+
+    NOTIFICATION_REGISTER_URL: str
+    NOTIFICATION_RESET_PASSWORD_URL: str
+
+    ELASTICSEARCH_HOST: str
+    ELASTICSEARCH_PORT: int
 
     class Config:
         env_file = ".env"
